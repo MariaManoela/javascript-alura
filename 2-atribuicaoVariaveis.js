4cedef68baa62c1149bbe95edcfe92a7c65062b1
Maria Manoela <manoelamvc@hotmail.com>
2022-05-30 19:52:59 -0300

    Add variable assignment

---
+++ 2-atribuicaoVariaveis.js
@@ -0,0 +1,8 @@
+console.log("Atribuição de variáveis");
+const idade = 21;
+const nome = "Manu";
+const sobrenome = "Cunha";
+
+console.log(nome + " " + sobrenome);
+console.log(nome, sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`)
