36cf4758dfa7094d7a23ea66db6f757608130425
Maria Manoela <manoelamvc@hotmail.com>
2022-05-31 08:23:54 -0300

    Add lists file

---
+++ 3-listas.js
@@ -0,0 +1,8 @@
+console.log(`Trabalhando com listas`);
+
+const listaDeDestinos = new Array(
+    `Salvador`, `São Paulo`, `Rio de Janeiro`
+);
+
+listaDeDestinos.push(`Porto Alegre`);
+console.log(listaDeDestinos);
