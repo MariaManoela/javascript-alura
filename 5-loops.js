ff0ec02afc81e21ed3a3b342f3cb4868b3256094
Maria Manoela <manoelamvc@hotmail.com>
2022-06-01 20:57:19 -0300

    Replacing while loop by for loop and adding one more conditional

--- 5-loops.js
+++ 5-loops.js
@@ -7,21 +7,22 @@ const listaDeDestinos = new Array(
 const idadeComprador = 14;
 const estaAcompanhada = true;
 let temPassagemComprada = false;
-const destino = "Curitiba";
+const destino = "Salvador";
 
 console.log("\nDestinos possíveis:");
 console.log(listaDeDestinos);
 
 const podeComprar = idadeComprador >= 18 || estaAcompanhada;
 
-let contador = 0;
 let destinoExiste = false;
-while(contador < 3) {
-    if(listaDeDestinos[contador] == destino) {
+for(let i = 0; i < listaDeDestinos.length; i++) {
+    if(listaDeDestinos[i] == destino) 
         destinoExiste = true;
-        break;
-    }
-    contador++;
 }
 
 console.log("Destino existe: ", destinoExiste);
+
+if(podeComprar && destinoExiste)
+    console.log("Boa viagem!");
+else
+    console.log("Desculpe, tivemos um erro");
