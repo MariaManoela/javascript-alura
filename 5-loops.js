adda62d05f2d676d875c137351ad1499c026cb56
Maria Manoela <manoelamvc@hotmail.com>
2022-06-01 20:38:52 -0300

    Removing else conditional and adding new variable

--- 5-loops.js
+++ 5-loops.js
@@ -7,7 +7,7 @@ const listaDeDestinos = new Array(
 const idadeComprador = 14;
 const estaAcompanhada = true;
 let temPassagemComprada = false;
-const destino = "Salvador";
+const destino = "Curitiba";
 
 console.log("\nDestinos possíveis:");
 console.log(listaDeDestinos);
@@ -15,11 +15,13 @@ console.log(listaDeDestinos);
 const podeComprar = idadeComprador >= 18 || estaAcompanhada;
 
 let contador = 0;
+let destinoExiste = false;
 while(contador < 3) {
     if(listaDeDestinos[contador] == destino) {
-        console.log("Destino existe!")
+        destinoExiste = true;
         break;
-    } else
-        console.log("Destino não existe!")
+    }
     contador++;
 }
+
+console.log("Destino existe: ", destinoExiste);
