afbb53093cc1b9b3f0ee03de1f694d9d261728a9
Maria Manoela <manoelamvc@hotmail.com>
2022-06-09 09:53:47 -0300

    Search for pacient weight in html file through js

--- programando-na-linguagem-da-web/introducao-javascript/js/principal.js
+++ programando-na-linguagem-da-web/introducao-javascript/js/principal.js
@@ -1,2 +1,10 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+console.log(paciente);
+console.log(peso);
