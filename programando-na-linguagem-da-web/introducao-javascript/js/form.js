249b6039d9fee8f54a676db5559518f144b83244
Maria Manoela <manoelamvc@hotmail.com>
2022-06-15 08:57:58 -0300

    Create montaTd function that create td elements and add to it patients values

--- programando-na-linguagem-da-web/introducao-javascript/js/form.js
+++ programando-na-linguagem-da-web/introducao-javascript/js/form.js
@@ -3,11 +3,8 @@ botaoAdicionar.addEventListener("click", function(event){
     event.preventDefault();
     
     var form = document.querySelector("#form-adiciona");
-    
     var paciente = obtemPacienteDoFormulario(form);
-
     var pacienteTr = montaTr(paciente);
-
     var tabela = document.querySelector("#tabela-pacientes");
 
     tabela.appendChild(pacienteTr);
@@ -27,18 +24,13 @@ function obtemPacienteDoFormulario(form) {
 
 function montaTr(paciente) {
     var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
 
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    nomeTd.textContent = paciente.nome;
-    pesoTd.textContent = paciente.peso;
-    alturaTd.textContent = paciente.altura;
-    gorduraTd.textContent = paciente.gordura;
-    imcTd.textContent = paciente.imc;
+    var nomeTd = montaTd(paciente.nome, "info-nome");
+    var pesoTd = montaTd(paciente.peso, "info-peso");
+    var alturaTd = montaTd(paciente.altura, "info-altura");
+    var gorduraTd = montaTd(paciente.gordura, "info-gordura");
+    var imcTd = montaTd(paciente.imc, "info-imc");
 
     pacienteTr.appendChild(nomeTd);
     pacienteTr.appendChild(pesoTd);
@@ -47,4 +39,12 @@ function montaTr(paciente) {
     pacienteTr.appendChild(imcTd);
 
     return pacienteTr;
+}
+
+function montaTd(dado, classe) {
+    var td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe);
+    
+    return td;
 }
