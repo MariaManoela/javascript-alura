4e032c4510b013368c98442504b74837cf613fa6
Maria Manoela <manoelamvc@hotmail.com>
2022-06-21 08:51:12 -0300

    Implement function that removes the table row that was clicked, including newly added patients

--- programando-na-linguagem-da-web/introducao-javascript/js/remover-paciente.js
+++ programando-na-linguagem-da-web/introducao-javascript/js/remover-paciente.js
@@ -1,7 +1,17 @@
 var pacientes = document.querySelectorAll(".paciente");
+var tabela = document.querySelector("#tabela-pacientes");
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick", function(){
-        this.remove();
-    });
+tabela.addEventListener("dblclick", function(event){
+    event.target.parentNode.remove();
+    //event.target -> campo que foi clicado
+    //parentNode -> pai do campo que foi clicado, que é o que queremos remover (<tr>)
 });
+
+
+// Essa função funciona, mas só nos elementos já existentes na página. Para conseguir remover os elementos adicionados à tabela, deve ser usado a função acima
+
+// pacientes.forEach(function(paciente){
+//     paciente.addEventListener("dblclick", function(){
+//         this.remove();
+//     });
+// });
