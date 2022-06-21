64a28bf9ebbf71efbdbc0d86de0cd1158c955005
Maria Manoela <manoelamvc@hotmail.com>
2022-06-21 09:06:31 -0300

    Add fadeOut css class to element that was double clicked and implement setTimeOut function to wait transition

--- programando-na-linguagem-da-web/introducao-javascript/js/remover-paciente.js
+++ programando-na-linguagem-da-web/introducao-javascript/js/remover-paciente.js
@@ -2,16 +2,12 @@ var pacientes = document.querySelectorAll(".paciente");
 var tabela = document.querySelector("#tabela-pacientes");
 
 tabela.addEventListener("dblclick", function(event){
-    event.target.parentNode.remove();
-    //event.target -> campo que foi clicado
-    //parentNode -> pai do campo que foi clicado, que é o que queremos remover (<tr>)
+    event.target.parentNode.classList.add("fadeOut");
+
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    }, 500);
 });
 
-
-// Essa função funciona, mas só nos elementos já existentes na página. Para conseguir remover os elementos adicionados à tabela, deve ser usado a função acima
-
-// pacientes.forEach(function(paciente){
-//     paciente.addEventListener("dblclick", function(){
-//         this.remove();
-//     });
-// });
+//event.target -> campo que foi clicado
+//parentNode -> pai do campo que foi clicado, que é o que queremos remover (<tr>)
