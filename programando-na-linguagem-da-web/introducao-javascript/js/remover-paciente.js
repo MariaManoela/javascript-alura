4071288059725187764872ace5fb4a69ce7254ea
Maria Manoela <manoelamvc@hotmail.com>
2022-06-21 08:29:30 -0300

    Implement remove method to the row that was double clicked

--- programando-na-linguagem-da-web/introducao-javascript/js/remover-paciente.js
+++ programando-na-linguagem-da-web/introducao-javascript/js/remover-paciente.js
@@ -3,5 +3,6 @@ var pacientes = document.querySelectorAll(".paciente");
 pacientes.forEach(function(paciente){
     paciente.addEventListener("dblclick", function(){
         console.log("Fui clicado duas vezes");
+        this.remove();
     });
 });
