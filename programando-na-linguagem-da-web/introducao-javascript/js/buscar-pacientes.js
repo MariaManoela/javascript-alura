8e657453b9ecfb8b91deab7b0519bb6154caa4f5
Maria Manoela <manoelamvc@hotmail.com>
2022-06-22 10:14:00 -0300

    Add forEach function to add new patients to the table through AJAX

--- programando-na-linguagem-da-web/introducao-javascript/js/buscar-pacientes.js
+++ programando-na-linguagem-da-web/introducao-javascript/js/buscar-pacientes.js
@@ -7,13 +7,11 @@ botaoAdicionar.addEventListener("click", function(){
     
     xhr.addEventListener("load", function(){
         var resposta = xhr.responseText;
-        console.log(resposta);
-        console.log(typeof resposta);
-
         var pacientes = JSON.parse(resposta);
-        console.log(pacientes);
-        console.log(typeof pacientes);
-
+        
+        pacientes.forEach(function(paciente){
+            adicionaPacienteNaTabela(paciente);
+        })
     });
 
     xhr.send();
