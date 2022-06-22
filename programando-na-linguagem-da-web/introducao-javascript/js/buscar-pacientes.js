38c74bdfd14ccbcc5ce41169eb3aec2f5b61b018
Maria Manoela <manoelamvc@hotmail.com>
2022-06-22 10:01:46 -0300

    Transform string into object through JSON.parse method

--- programando-na-linguagem-da-web/introducao-javascript/js/buscar-pacientes.js
+++ programando-na-linguagem-da-web/introducao-javascript/js/buscar-pacientes.js
@@ -6,8 +6,15 @@ botaoAdicionar.addEventListener("click", function(){
     xhr.open("GET", "http://api-pacientes.herokuapp.com/pacientes");
     
     xhr.addEventListener("load", function(){
-        console.log(xhr.responseText);
+        var resposta = xhr.responseText;
+        console.log(resposta);
+        console.log(typeof resposta);
+
+        var pacientes = JSON.parse(resposta);
+        console.log(pacientes);
+        console.log(typeof pacientes);
+
     });
-    
+
     xhr.send();
 });
