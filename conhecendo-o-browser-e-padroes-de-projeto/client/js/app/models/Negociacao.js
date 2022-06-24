44d80482d1615c55b6438d9e41961e619d0202e9
Maria Manoela <manoelamvc@hotmail.com>
2022-06-24 08:44:00 -0300

    Create getters

--- conhecendo-o-browser-e-padroes-de-projeto/client/js/app/models/Negociacao.js
+++ conhecendo-o-browser-e-padroes-de-projeto/client/js/app/models/Negociacao.js
@@ -6,7 +6,19 @@ class Negociacao {
         this._valor = valor;
     }
 
-    obtemVolume() {
+    getVolume() {
         return this._quantidade * this._valor;
+    }
+
+    getData() {
+        return this._data;
+    }
+
+    getQuantidade() {
+        return this._quantidade;
+    }
+
+    getValor() {
+        return this._valor;
     }
 }
