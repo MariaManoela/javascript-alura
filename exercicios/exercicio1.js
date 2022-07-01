0f5607114301ea0a2637f356ac7444fb448cc1eb
Maria Manoela <manoelamvc@hotmail.com>
2022-07-01 10:01:47 -0300

    Add other ways to do the exercise

--- exercicios/exercicio1.js
+++ exercicios/exercicio1.js
@@ -4,6 +4,7 @@
 
 // Sua tarefa: criar uma nova lista com a mesma quantidade de números, mas cada elemento da nova lista deve ter seu valor dobrado quando for ímpar.
 
+// 1 forma de fazer
 let numeros = [3,2,11,20,8,7];
 let novosNumeros = [];
 
@@ -13,5 +14,14 @@ numeros.forEach((item) => {
     else
         novosNumeros.push(item);
 });
-
 console.log(novosNumeros);
+
+// 2 forma de fazer
+let numeros2 = [3,2,11,20,8,7];
+let novosNumeros2 = numeros2.map(item => item % 2 ? item * 2 : item);
+console.log(novosNumeros2);
+
+// 3 forma de fazer
+let numeros3 = [3,2,11,20,8,7];
+let novosNumeros3 = numeros.map((item) => (item % 2 + 1) * item);
+console.log(novosNumeros3);
