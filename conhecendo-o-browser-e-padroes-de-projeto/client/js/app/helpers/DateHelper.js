4f555074c1ea70816ce0237d556d57583baba223
Maria Manoela <manoelamvc@hotmail.com>
2022-07-04 08:26:40 -0300

    Turn methods static to allow the access to methods without creating an object

--- conhecendo-o-browser-e-padroes-de-projeto/client/js/app/helpers/DateHelper.js
+++ conhecendo-o-browser-e-padroes-de-projeto/client/js/app/helpers/DateHelper.js
@@ -1,12 +1,12 @@
 class DateHelper {
 
-    dataParaTexto(data) {
-        return data.data.getDate() 
-            + '/' + (data.data.getMonth() + 1)
-            + '/' + data.data.getFullYear();
+    static dataParaTexto(data) {
+        return data.getDate() 
+            + '/' + (data.getMonth() + 1)
+            + '/' + data.getFullYear();
     }
 
-    textoParaData(texto) {
+    static textoParaData(texto) {
         return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
     }
 }
