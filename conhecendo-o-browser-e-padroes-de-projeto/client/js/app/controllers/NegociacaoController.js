fbfc0e80e6a957ce7a7335664dcd68c214cfe59c
Maria Manoela <manoelamvc@hotmail.com>
2022-07-08 08:47:29 -0300

    Show message when submit the form

--- conhecendo-o-browser-e-padroes-de-projeto/client/js/app/controllers/NegociacaoController.js
+++ conhecendo-o-browser-e-padroes-de-projeto/client/js/app/controllers/NegociacaoController.js
@@ -11,15 +11,18 @@ class NegociacaoController  {
         this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView();
-
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
     }
 
     adiciona(event) {
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._mensagem.texto = 'Negociação adicionada com sucesso!';
         this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem.texto = 'Negociação adicionada com sucesso!';
+        this._mensagemView.update(this._mensagem);
+        
         this._limpaFormulario();
     }
 
