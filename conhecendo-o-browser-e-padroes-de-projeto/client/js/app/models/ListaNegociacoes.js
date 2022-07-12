e951cfb858a0bb5ee7c9b13be3c7d7d58d5333cb
Maria Manoela <manoelamvc@hotmail.com>
2022-07-12 08:18:40 -0300

    Remove Reflect and implement arrow function due to lexical scope

--- conhecendo-o-browser-e-padroes-de-projeto/client/js/app/models/ListaNegociacoes.js
+++ conhecendo-o-browser-e-padroes-de-projeto/client/js/app/models/ListaNegociacoes.js
@@ -1,16 +1,13 @@
 class ListaNegociacoes {
 
-    constructor(contexto, armadilha) {
+    constructor(armadilha) {
         this._negociacoes = [];
         this._armadilha = armadilha;
-        this._contexto = contexto;
     }
 
     adiciona(negociacao) {
         this._negociacoes.push(negociacao);
-        //this._armadilha(this);
-        Reflect.apply(this._armadilha, this._contexto, [this]);
-        //          função desejada, contexto desejado, [parâmetros passados para função]
+        this._armadilha(this);
     }
 
     get negociacoes() {
@@ -19,7 +16,6 @@ class ListaNegociacoes {
 
     esvazia() {
         this._negociacoes = [];
-       // this._armadilha(this);
-       Reflect.apply(this._armadilha, this._contexto, [this]);
+        this._armadilha(this);
     }
 }
